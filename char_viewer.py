2e495896c61b1471ad22c99e99a673752b38fbd3
L0mc <meichenl404@outlook.com>
2026-06-22 18:35:55 +0800

    fix: disable Flask debug mode in char_viewer to prevent reloader issue

--- char_viewer.py
+++ char_viewer.py
@@ -235,4 +235,4 @@ def shutdown():
     return jsonify({'ok': True, 'm': '服务器即将退出'})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host='127.0.0.1', port=5001, debug=False)
